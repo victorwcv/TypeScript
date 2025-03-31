fc47b02e366f699bfe088bd1becf6b0bba982c0f
William <victorwccv@gmail.com>
2025-03-30 22:47:37 -0500

    Generics_2

--- Bases_TS/generics.ts
+++ Bases_TS/generics.ts
@@ -1,9 +1,27 @@
 (() => {
-	const printObject = <T>(algo: T): T => {
+	const genericFunction = <T>(algo: T): T => {
 		return algo;
 	};
 
-	let a = printObject(123)
-	printObject([1, 2, 3]);
-	printObject("Hola mundo")
+	let a = genericFunction(123);
+	genericFunction([1, 2, 3]);
+	genericFunction("Hola mundo");
+
+	interface Hero {
+		name: string;
+		realName: string;
+	}
+
+	interface Villian {
+		name: string;
+		dangerLevel: number;
+	}
+
+	const deadpool = {
+		name: "Deadpool",
+		realName: "Wade Winson",
+		dangerLevel: 130,
+	};
+
+	console.log(genericFunction<Villian>(deadpool));
 })();
